46a74e6c3a2da66696e897317bbc1e4ff45c7e2c
jmaillot <jeremymaillot@gmail.com>
2023-08-10 17:04:09 +0200

    chore:(README.md)

---
+++ azure-base-infrastructure/Remove_Terraform_StateFiles.ps1
@@ -0,0 +1,5 @@
+$CurrentDirectory = Get-Location
+Write-Host "Our current Working Directory is $CurrentDirectory"
+
+Get-ChildItem -Path $CurrentDirectory -Include *.tfstate*,*.tfplan,*.lock.* -File -Recurse | foreach { echo "Deleting: $_" ; $_.Delete()}
+
