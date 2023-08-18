e2e24a31c2b8a834f41caa23510093acd3c6550f
jmaillot <jeremymaillot@gmail.com>
2023-08-18 15:13:01 +0200

    fix:(Output VM ID for Azure Backup module)

--- azure-base-infrastructure/modules/virtual-machine/outputs.tf
+++ azure-base-infrastructure/modules/virtual-machine/outputs.tf
@@ -2,3 +2,7 @@ output "admin_password" {
   sensitive = true
   value     = azurerm_windows_virtual_machine.vm.admin_password
 }
+
+output "vm_id" {
+  value     = azurerm_windows_virtual_machine.vm.id
+}
